ef9d52bf3358a1d20417d8c26405fe122f8e1569
Lory-Stan TANASI <stantanasi@gmail.com>
2022-06-23 14:37:44 +0200

    feat: refresh list on duplicate

--- src/pages/ListProject.js
+++ src/pages/ListProject.js
@@ -12,7 +12,7 @@ const ListProject = () => {
   const [data, setData] = useState([{}]);
   const [isOpen, setIsOpen] = useState(false)
   const [selectedId, setSelectedId] = useState('')
-  
+
 
   const togglePopup = (id) => {
     console.log(id);
@@ -47,27 +47,26 @@ const ListProject = () => {
   }
 
 
-const duplicate =(id)=>{
+  const duplicate = (id) => {
+    fetch(`/project/${id}/copy`)
+      .then(res => res.json())
+      .then(() => {
+        fetch("/projects")
+          .then((response) => response.json())
+          .then(({ projects }) => {
+            setData(projects)
+          })
+      })
+  }
 
-    fetch (`/project/${id}/copy`).then(
-    res=>res.json()
+  useEffect(() => {
+    fetch("/projects").then(
+      res => res.json()
     ).then(
-      data =>{
-      data =>data.projects
-      setData(data)
-      navigate('/');
-    }
-    )
-}
-
- useEffect(() => {
-  fetch ("/projects").then(
-    res => res.json()
-  ).then(
-    data => {
+      data => {
         data = data.projects
         setData(data)
-        
+
       }
     )
   }, []);
@@ -76,28 +75,28 @@ const duplicate =(id)=>{
       <div>
         <button className="new-project" onClick={btn1_Click}>New Projects</button>
       </div>
-        <ul className='list-project'>
-            {data.map((item, index) => {
-             
-              return (
-                <li key={index} className="list">
-                  <li>Id : #{item.id}</li>
-                  <li>Name project : {item.name}</li>
-                  <button className='display' onClick={()=>togglePopup(item.id)}>Afficher plus</button>
-                  <button className='copy' onClick={()=> duplicate(item.id)}>Dupliquer</button>
-                  <button className='delete' onClick={() => deleteProject(item.id)}>supprimer</button> 
-              <button className='export' onClick={() => exportProject(item.id)}>Extraire</button> 
-                </li>
-              );
-            })}
-             {isOpen && (
-                <PopUp
-                    id={selectedId}
-                    handleClose={togglePopup}
-                    isOpen={isOpen}
-                />
-            )}
-          </ul>
+      <ul className='list-project'>
+        {data.map((item, index) => {
+
+          return (
+            <li key={index} className="list">
+              <li>Id : #{item.id}</li>
+              <li>Name project : {item.name}</li>
+              <button className='display' onClick={() => togglePopup(item.id)}>Afficher plus</button>
+              <button className='copy' onClick={() => duplicate(item.id)}>Dupliquer</button>
+              <button className='delete' onClick={() => deleteProject(item.id)}>supprimer</button>
+              <button className='export' onClick={() => exportProject(item.id)}>Extraire</button>
+            </li>
+          );
+        })}
+        {isOpen && (
+          <PopUp
+            id={selectedId}
+            handleClose={togglePopup}
+            isOpen={isOpen}
+          />
+        )}
+      </ul>
     </div>
   )
 }
